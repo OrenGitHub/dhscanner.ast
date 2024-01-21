e15365e40bda2c8111eaf2d75f795aced8f24e46
OrenGitHub <tuna.is.good.for.you@gmail.com>
2024-01-21 15:14:10 +0200

    minor improvements

--- src/Token.hs
+++ src/Token.hs
@@ -11,34 +11,34 @@ import Location
 import Data.Aeson
 import GHC.Generics
 
-data Named
-   = Named
+data Token
+   = Token
    {
        content :: String,
-       locationNamed :: Location
+       location :: Location
    }
    deriving ( Show, Eq, Generic, ToJSON, FromJSON, Ord )
    
-data VarName   = VarName   Named deriving ( Show, Eq, Generic, ToJSON, FromJSON, Ord )
-data FuncName  = FuncName  Named deriving ( Show, Eq, Generic, ToJSON, FromJSON, Ord )
-data ParamName = ParamName Named deriving ( Show, Eq, Generic, ToJSON, FromJSON, Ord )
-data FieldName = FieldName Named deriving ( Show, Eq, Generic, ToJSON, FromJSON, Ord )
-data ClassName = ClassName Named deriving ( Show, Eq, Generic, ToJSON, FromJSON, Ord )
-data SuperName = SuperName Named deriving ( Show, Eq, Generic, ToJSON, FromJSON, Ord )
-data MembrName = MembrName Named deriving ( Show, Eq, Generic, ToJSON, FromJSON, Ord, ToJSONKey, FromJSONKey )
-data MethdName = MethdName Named deriving ( Show, Eq, Generic, ToJSON, FromJSON, Ord, ToJSONKey, FromJSONKey )
-data NominalTy = NominalTy Named deriving ( Show, Eq, Generic, ToJSON, FromJSON, Ord )
+data VarName   = VarName   Token deriving ( Show, Eq, Generic, ToJSON, FromJSON, Ord )
+data FuncName  = FuncName  Token deriving ( Show, Eq, Generic, ToJSON, FromJSON, Ord )
+data ParamName = ParamName Token deriving ( Show, Eq, Generic, ToJSON, FromJSON, Ord )
+data FieldName = FieldName Token deriving ( Show, Eq, Generic, ToJSON, FromJSON, Ord )
+data ClassName = ClassName Token deriving ( Show, Eq, Generic, ToJSON, FromJSON, Ord )
+data SuperName = SuperName Token deriving ( Show, Eq, Generic, ToJSON, FromJSON, Ord )
+data MembrName = MembrName Token deriving ( Show, Eq, Generic, ToJSON, FromJSON, Ord, ToJSONKey, FromJSONKey )
+data MethdName = MethdName Token deriving ( Show, Eq, Generic, ToJSON, FromJSON, Ord, ToJSONKey, FromJSONKey )
+data NominalTy = NominalTy Token deriving ( Show, Eq, Generic, ToJSON, FromJSON, Ord )
 
 -- signatures (to quiet ghc -Wall)
-getVarNameToken   :: VarName   -> Named
-getFuncNameToken  :: FuncName  -> Named
-getParamNameToken :: ParamName -> Named
-getFieldNameToken :: FieldName -> Named
-getClassNameToken :: ClassName -> Named
-getSuperNameToken :: SuperName -> Named
-getMembrNameToken :: MembrName -> Named
-getMethdNameToken :: MethdName -> Named
-getNominalTyToken :: NominalTy -> Named
+getVarNameToken   :: VarName   -> Token
+getFuncNameToken  :: FuncName  -> Token
+getParamNameToken :: ParamName -> Token
+getFieldNameToken :: FieldName -> Token
+getClassNameToken :: ClassName -> Token
+getSuperNameToken :: SuperName -> Token
+getMembrNameToken :: MembrName -> Token
+getMethdNameToken :: MethdName -> Token
+getNominalTyToken :: NominalTy -> Token
 
 -- getters
 getVarNameToken   (VarName   v) = v
@@ -50,6 +50,10 @@ getSuperNameToken (SuperName s) = s
 getMembrNameToken (MembrName m) = m
 getMethdNameToken (MethdName m) = m
 getNominalTyToken (NominalTy t) = t
+
+-- locations
+getSrcVariableLocation :: VarName -> Location
+getSrcVariableLocation = location . getVarNameToken
 
 data ConstInt
    = ConstInt
